dcf65e2885314e32f07f027e19bbce116484e2f0
nb48 <nathan.john.blades@gmail.com>
2017-09-02 18:49:20 +0100

    feat: Add bpm preview

--- src/app/audio-store/audio-store.service.ts
+++ src/app/audio-store/audio-store.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable, ReplaySubject } from 'rxjs';
 
 @Injectable()
 export class AudioStoreService {
 
   private _file: File;
   private _audio: HTMLAudioElement;
+  private _currentTime: ReplaySubject<number>;
+  private _frame: number;
 
   constructor() {
+    this._currentTime = new ReplaySubject<number>();
   }
 
   get name(): string {
@@ -15,15 +19,36 @@ export class AudioStoreService {
 
   set file(file: File) {
     this._file = file;
-  }
-
-  play() {
     this._audio = new Audio();
     this._audio.src = URL.createObjectURL(this._file);
-    this._audio.play();
+    this._audio.load();
   }
 
-  stop() {
+  get currentTime(): Observable<number> {
+    return this._currentTime.asObservable();
+  }
+
+  get duration(): number {
+    return this._audio.duration;
+  }
+
+  start(): void {
+    this._audio.play();
+    this._frame = window.setInterval(() => this.frame(), 10);
+  }
+
+  stop(): void {
     this._audio.pause();
+    window.clearInterval(this._frame);
+    this._frame = undefined;
+  }
+
+  private frame(): void {
+    if (this._audio.currentTime) {
+      this._currentTime.next(this._audio.currentTime);
+    }
+    if (this._audio.ended) {
+      this.stop();
+    }
   }
 }
--- src/app/chart-preview/chart-preview.component.ts
+++ src/app/chart-preview/chart-preview.component.ts
@@ -2,6 +2,49 @@ import { Component, OnInit } from '@angular/core';
 
 import { AudioStoreService } from '../audio-store/audio-store.service';
 
+interface Beat {
+  time: number;
+  visible: boolean;
+  position: number;
+}
+
+interface ViewWindow {
+  earliest: number;
+  latest: number;
+  zeroPosition: number;
+}
+
+const buildBeats = (start: number, duration: number, bpm: number, offset: number): Beat[] => {
+  let beats = [];
+  let time = start + offset;
+  while (time < duration) {
+    time += 60 / bpm;
+    beats.push({
+      time: time,
+      visible: false,
+      position: undefined
+    });
+  }
+  return beats;
+};
+
+const updateBeats = (beats: Beat[], currentTime: number, viewWindow: ViewWindow): void => {
+  const earliest = currentTime + viewWindow.earliest;
+  const latest = currentTime + viewWindow.latest;
+  beats.forEach((beat) => {
+    if (beat.time < earliest && beat.time > latest) {
+      beat.visible = true;
+      beat.position = (earliest - beat.time) / (earliest - latest) * 100;
+    } else {
+      beat.visible = false;
+      beat.position = undefined;
+    }
+  });
+};
+
+const earliest = 2.8;
+const latest = -0.4;
+
 @Component({
   selector: 'app-chart-preview',
   templateUrl: './chart-preview.component.html',
@@ -9,9 +52,31 @@ import { AudioStoreService } from '../audio-store/audio-store.service';
 })
 export class ChartPreviewComponent implements OnInit {
 
+  bpm: number = 150;
+  offset: number = 0.247;
+  viewWindow: ViewWindow = {
+    earliest: earliest,
+    latest: latest,
+    zeroPosition: earliest / (earliest - latest) * 100
+  };
+
+  beats: Beat[];
+
   constructor(public audioStore: AudioStoreService) {
   }
 
   ngOnInit() {
+    this.audioStore.currentTime.subscribe((currentTime: number) => {
+      updateBeats(this.beats, currentTime, this.viewWindow);
+    });
+  }
+
+  start() {
+    this.beats = buildBeats(this.viewWindow.latest, this.audioStore.duration, this.bpm, this.offset);
+    this.audioStore.start();
+  }
+
+  stop() {
+    this.audioStore.stop();
   }
 }

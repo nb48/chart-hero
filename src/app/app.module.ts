c6904dd4a39943633b5ec6e28d6d3ec274df1dae
nb48 <nathan.john.blades@gmail.com>
2017-09-02 02:04:07 +0100

    feat: Add title and sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { MdButtonModule, MdListModule, MdSidenavModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdListModule,
+    MdSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]

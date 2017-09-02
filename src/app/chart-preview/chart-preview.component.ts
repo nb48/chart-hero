163c23027f3eb903b8a79028579c165f05e42af5
nb48 <nathan.john.blades@gmail.com>
2017-09-02 21:49:02 +0100

    feat: Add notes to chart preview

--- src/app/chart-preview/chart-preview.component.ts
+++ src/app/chart-preview/chart-preview.component.ts
@@ -7,14 +7,68 @@ interface Beat {
   position: number;
 }
 
+interface ChartNote {
+  time: number;
+  color: string;
+}
+
+interface Note {
+  x: number;
+  y: number;
+  color: string;
+}
+
 interface ViewWindow {
   earliest: number;
   latest: number;
   zeroPosition: number;
 }
 
-const earliest = 2.8;
-const latest = -0.4;
+const earliest: number = 2.8;
+const latest: number = -0.4;
+
+const chart: ChartNote[] = [{
+  time: 0.647,
+  color: 'green'
+}, {
+  time: 1.047,
+  color: 'red'
+}, {
+  time: 1.447,
+  color: 'yellow'
+}, {
+  time: 1.847,
+  color: 'blue'
+}, {
+  time: 2.247,
+  color: 'orange'
+}];
+
+const buildNote = (color: string, y: number): Note => {
+  let x = undefined;
+  switch (color) {
+    case 'green':
+      x = 10;
+      break;
+    case 'red':
+      x = 30;
+      break;
+    case 'yellow':
+      x = 50;
+      break;
+    case 'blue':
+      x = 70;
+      break;
+    case 'orange':
+      x = 90;
+      break;
+  }
+  return {
+    x: x,
+    y: y,
+    color: color
+  }
+}
 
 @Component({
   selector: 'app-chart-preview',
@@ -33,13 +87,17 @@ export class ChartPreviewComponent implements OnInit {
 
   beats: Beat[];
 
+  notes: Note[];
+
   constructor(public audioStore: AudioStoreService) {
     this.beats = this.buildBeats(0);
+    this.notes = this.buildNotes(0);
   }
 
   ngOnInit() {
     this.audioStore.currentTime.subscribe((currentTime: number) => {
-      this.beats = this.buildBeats(currentTime)
+      this.beats = this.buildBeats(currentTime);
+      this.notes = this.buildNotes(currentTime);
     });
   }
 
@@ -50,6 +108,7 @@ export class ChartPreviewComponent implements OnInit {
   stop(): void {
     this.audioStore.stop(0);
     this.beats = this.buildBeats(0);
+    this.notes = this.buildNotes(0);
   }
 
   private buildBeats(currentTime: number): Beat[] {
@@ -68,5 +127,22 @@ export class ChartPreviewComponent implements OnInit {
       });
     }
     return beats;
+  }
+
+  private buildNotes(currentTime: number): Note[] {
+    const earliest = currentTime + this.viewWindow.earliest;
+    const latest = currentTime + this.viewWindow.latest;
+    let notes = [];
+    let visibleIndex = chart.findIndex((note) => note.time > latest); 
+    while (true) {
+      const note = chart[visibleIndex];
+      if (!note || note.time > earliest) {
+        break;
+      }
+      const y = (earliest - note.time) / (earliest - latest) * 100;
+      notes.push(buildNote(note.color, y));
+      visibleIndex += 1;
+    }
+    return notes;
   }
 }

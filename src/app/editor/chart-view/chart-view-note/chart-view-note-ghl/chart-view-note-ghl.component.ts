b982e79b3d3f41a0dd4ad61c1d68a151ce4c5700
nb48 <nathan.john.blades@gmail.com>
2017-10-15 17:52:44 +0100

    feat: Update note SVG designs

--- src/app/editor/chart-view/chart-view-note/chart-view-note-ghl/chart-view-note-ghl.component.ts
+++ src/app/editor/chart-view/chart-view-note/chart-view-note-ghl/chart-view-note-ghl.component.ts
@@ -22,7 +22,7 @@ export class ChartViewNoteGHLComponent {
     }
 
     get blackNotePath(): string {
-        return `M ${this.note.x + 1.8} ${this.note.y - 7.75}
+        return `M 1.5 -4
                 l 2.8 4.7
                 s 1.3 3 -1.82 3.22
                 l -5.4 0
@@ -33,41 +33,41 @@ export class ChartViewNoteGHLComponent {
     }
 
     get whiteNotePath(): string {
-        return `M ${this.note.x + 1.8} ${this.note.y - 0.9}
-            l 2.8 -4.7
-            s 1.3 -3 -1.82 -3.22
-            l -5.4 0
-            s -3.28 0.14 -1.74 3.26
-            l 2.76 4.7
-            s 1.7 2.3 3.4 0
-            Z`;
-    }
-
-    get halfBlackNotePath(): string {
-        return `
-            M ${this.note.x} ${this.note.y - 4}
-            ${this.roundedCorner(-4.5, 4)}
-            ${this.roundedCorner(4.5, 4)}
-            Z
-        `;
+        return `M 1.5 4
+                l 2.8 -4.7
+                s 1.3 -3 -1.82 -3.22
+                l -5.4 0
+                s -3.28 0.14 -1.74 3.26
+                l 2.76 4.7
+                s 1.7 2.3 3.4 0
+                Z`;
     }
 
     get halfWhiteNotePath(): string {
-        return `
-            M ${this.note.x} ${this.note.y - 4}
-            ${this.roundedCorner(-4.5, -4)}
-            ${this.roundedCorner(4.5, -4)}
-            Z
-        `;
+        return `M 0 0
+                ${this.roundedCorner(-4.5, 4)}
+                ${this.roundedCorner(4.5, 4)}
+                Z`;
+    }
+
+    get halfBlackNotePath(): string {
+        return `M 0 0
+                ${this.roundedCorner(-4.5, -4)}
+                ${this.roundedCorner(4.5, -4)}
+                Z`;
+    }
+
+    noteTransform(scale: number, x: number, y: number): string {
+        const translateX = (this.note.x + x) / scale;
+        const translateY = (this.note.y + y) / scale;
+        return `scale(${scale}) translate(${translateX},${translateY})`;
     }
 
     private roundedCorner(x: number, y: number): string {
-        return `
-            l ${x} 0
-            l 0 ${0.5 * y}
-            s 0 ${0.5 * y} ${-0.7 * x} ${0.5 * y}
-            l ${-0.3 * x} 0
-            l 0 ${-y}
-        `;     
+        return `l ${x} 0
+                l 0 ${0.5 * y}
+                s 0 ${0.5 * y} ${-0.7 * x} ${0.5 * y}
+                l ${-0.3 * x} 0
+                l 0 ${-y}`;     
     }
 }

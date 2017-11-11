e455446cfe9c7e1c3e3076f56fc7e2f927364f30
nb48 <nathan.john.blades@gmail.com>
2017-11-11 18:56:50 +0000

    feat: Add tooltips to event component

--- src/app/fretboard/event/event.component.ts
+++ src/app/fretboard/event/event.component.ts
@@ -5,7 +5,15 @@ import { Event } from './event';
 @Component({
     selector: '[app-event]',
     templateUrl: './event.component.html',
+    styleUrls: ['./event.component.css'],
 })
 export class EventComponent {
     @Input() event: Event;
+
+    select(): void {
+    }
+
+    get tooltip(): string {
+        return `${this.event.id}, ${Math.floor(this.event.time * 1000) / 1000}`;
+    }
 }

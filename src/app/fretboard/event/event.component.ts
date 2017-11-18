29ee1f488b11d42f817174abd0a18ba4b1178415
nb48 <nathan.john.blades@gmail.com>
2017-11-18 19:54:34 +0000

    feat: Disable fretboard tooltips while playing, and add snap to event time by double clicking event

--- src/app/fretboard/event/event.component.ts
+++ src/app/fretboard/event/event.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 
 import { SelectorService } from '../../controller/selector/selector.service';
 import { showTime } from '../../time/audio-player-controls/audio-player-controls.component';
+import { TimeService } from '../../time/time.service';
 import { Event } from './event';
 
 @Component({
@@ -12,11 +13,28 @@ import { Event } from './event';
 export class EventComponent {
     @Input() event: Event;
 
-    constructor(private selectorService: SelectorService) {
+    constructor(
+        private selectorService: SelectorService,
+        private timeService: TimeService,
+    ) {
+    }
+
+    get playing(): boolean {
+        return this.timeService.playing;
     }
 
     select(event: any): void {
-        this.selectorService.selectEvent(this.event.id);
+        if (!this.timeService.playing) {
+            this.selectorService.selectNote(this.event.id);            
+        }
+        event.stopPropagation();
+    }
+
+    selectAndSnap(event: any): void {
+        if (!this.timeService.playing) {
+            this.selectorService.selectNote(this.event.id);
+            this.timeService.time = this.event.time;            
+        }
         event.stopPropagation();
     }
 

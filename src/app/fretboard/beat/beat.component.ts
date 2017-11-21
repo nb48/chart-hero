50ab47c6b8fb074f9587db1f5c119830cab417c6
nb48 <nathan.john.blades@gmail.com>
2017-11-21 19:22:48 +0000

    feat: Snap to beat time by double clicking beat

--- src/app/fretboard/beat/beat.component.ts
+++ src/app/fretboard/beat/beat.component.ts
@@ -1,11 +1,32 @@
 import { Component, Input } from '@angular/core';
 
+import { showTime } from '../../time/audio-player-controls/audio-player-controls.component';
+import { TimeService } from '../../time/time.service';
 import { Beat } from './beat';
 
 @Component({
     selector: '[app-beat]',
     templateUrl: './beat.component.html',
+    styleUrls: ['./beat.component.css'],    
 })
 export class BeatComponent {
     @Input() beat: Beat;
+
+    constructor(private timeService: TimeService) {
+    }
+
+    get playing(): boolean {
+        return this.timeService.playing;
+    }
+
+    selectAndSnap(event: any): void {
+        if (!this.timeService.playing) {
+            this.timeService.time = this.beat.time;            
+        }
+        event.stopPropagation();
+    }
+
+    get tooltip(): string {
+        return `${showTime(this.beat.time)}`;
+    }
 }

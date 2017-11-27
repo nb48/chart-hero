cc8b26ccaf79b26f64c27ba27d0ef7242eaf1c03
nb48 <nathan.john.blades@gmail.com>
2017-11-27 23:40:12 +0000

    refactor: Reset idGenerator in model import instead of file select

--- src/app/model/import-export/model-importer.service.ts
+++ src/app/model/import-export/model-importer.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { SelectorService } from '../../controller/selector/selector.service';
 import { TimeService } from '../../time/time.service';
 import { TrackService } from '../../track/track.service';
+import { IdGeneratorService } from '../id-generator/id-generator.service';
 import { ModelService } from '../model.service';
 import { FileToMemoryService } from './file-to-memory/file-to-memory.service';
 import { MemoryToModelService } from './memory-to-model/memory-to-model.service';
@@ -14,6 +15,7 @@ export class ModelImporterService {
         private selectorService: SelectorService,
         private timeService: TimeService,
         private trackService: TrackService,
+        private idGenerator: IdGeneratorService,
         private modelService: ModelService,
         private fileToMemory: FileToMemoryService,
         private memoryToModel: MemoryToModelService,
@@ -22,6 +24,7 @@ export class ModelImporterService {
     }
 
     import(file: string): void {
+        this.idGenerator.reset();
         const memory = this.fileToMemory.import(file);
         const model = this.memoryToModel.import(memory);
         this.timeService.time = 0;

76cdd2f0af1b6604b180f199a0dac6ceb695e09f
nb48 <nathan.john.blades@gmail.com>
2017-12-10 22:56:35 +0000

    feat: Add tap input time display and actions

--- src/app/tap-input/display/tap-display.component.ts
+++ src/app/tap-input/display/tap-display.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
-import { TapInputService } from '../tap-input.service';
+import { showTime } from '../../time/audio-player-controls/audio-player-controls.component';
+import { TapInputService, TapInputTime } from '../tap-input.service';
 
 @Component({
     selector: 'app-tap-display',
@@ -9,6 +10,31 @@ import { TapInputService } from '../tap-input.service';
 })
 export class TapDisplayComponent {
 
+    times: TapInputTime[];
+
     constructor(private service: TapInputService) {
+        this.service.times.subscribe((times) => {
+            this.times = times;
+        });
+    }
+
+    captureScroll(event: Event): void {
+        event.stopPropagation();
+    }
+
+    showTime(time: TapInputTime): string {
+        return showTime(time.time);
+    }
+
+    selectAll(): void {
+        this.times.forEach(time => time.selected = true);
+    }
+
+    deselectAll(): void {
+        this.times.forEach(time => time.selected = false);        
+    }
+
+    createNotes(): void {
+        this.service.createNotes();
     }
 }

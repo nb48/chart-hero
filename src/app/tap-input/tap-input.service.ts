c488ffb97f29f1e00029de417e74ffc2ab3980f0
nb48 <nathan.john.blades@gmail.com>
2017-12-10 22:56:35 +0000

    feat: Add tap input time display and actions

--- src/app/tap-input/tap-input.service.ts
+++ src/app/tap-input/tap-input.service.ts
@@ -1,21 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 import { TimeService } from '../time/time.service';
+
+export interface TapInputTime {
+    time: number;
+    selected: boolean;
+}
 
 @Injectable()
 export class TapInputService {
 
+    private timesSubject: BehaviorSubject<TapInputTime[]>;
     private currentTime: number;
-    private times: number[];
 
     constructor(private timeService: TimeService) {
+        this.timesSubject = new BehaviorSubject<TapInputTime[]>([]);
         this.timeService.times.subscribe((time) => {
             this.currentTime = time;
         });
-        this.times = [];
+    }
+
+    get times(): Observable<TapInputTime[]> {
+        return this.timesSubject.asObservable();
     }
 
     addTime(): void {
-        this.times.push(this.currentTime);
+        this.timesSubject.next([...this.timesSubject.value, {
+            time: this.currentTime,
+            selected: false,
+        }]);
+    }
+
+    createNotes(): void {
+        console.log('Create notes');
     }
 }

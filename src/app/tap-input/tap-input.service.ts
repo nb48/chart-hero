b1ec7df9445e3e36bea404029407bf3da94ea682
nb48 <nathan.john.blades@gmail.com>
2017-12-10 21:53:27 +0000

    feat: Store time after tap input

--- src/app/tap-input/tap-input.service.ts
+++ src/app/tap-input/tap-input.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 
+import { TimeService } from '../time/time.service';
+
 @Injectable()
 export class TapInputService {
 
+    private currentTime: number;
+    private times: number[];
+
+    constructor(private timeService: TimeService) {
+        this.timeService.times.subscribe((time) => {
+            this.currentTime = time;
+        });
+        this.times = [];
+    }
+
     addTime(): void {
-        console.log('Add time');
+        this.times.push(this.currentTime);
     }
 }

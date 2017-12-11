6811b7b9e0f30f31c8d2491037ecc5ba88542b0f
nb48 <nathan.john.blades@gmail.com>
2017-12-11 22:08:59 +0000

    feat: Display keybindings in modal

--- src/app/global/modals/keybindings/keybindings-modal.component.ts
+++ src/app/global/modals/keybindings/keybindings-modal.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
+import { Subscription } from 'rxjs';
+
+import { KeybindingsService, Keybinding } from '../../keybindings/keybindings.service';
 
 @Component({
     selector: 'app-keybindings-modal',
     templateUrl: './keybindings-modal.component.html',
     styleUrls: ['./keybindings-modal.component.css'],
 })
-export class KeybindingsModalComponent {
+export class KeybindingsModalComponent implements OnDestroy {
 
-    constructor(private dialogRef: MatDialogRef<KeybindingsModalComponent>) {
+    keybindings: Keybinding[];
+    subscription: Subscription;
+
+    constructor(
+        private dialogRef: MatDialogRef<KeybindingsModalComponent>,
+        private keybindingsService: KeybindingsService,
+    ) {
+        this.subscription = this.keybindingsService.keybindings.subscribe((keybindings) => {
+            this.keybindings = keybindings;
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
     }
 
     close(): void {
